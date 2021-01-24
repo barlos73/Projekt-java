f5a1ba83a7fac48eb969343addc015cf30d00db8
Bartek <Bartek@DESKTOP-QRR7V76>
2021-01-24 20:11:49 +0100

    Update, zliczanie

---
+++ ProjektJava/src/main/java/Projekt.java
@@ -0,0 +1,43 @@
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Bartek
+ */
+public class Projekt {
+
+    /**
+     * @param args the command line arguments
+     */
+ public static void main(String[] args) throws FileNotFoundException, IOException {
+
+      System.out.println("Podaj scierzke do pliku: ");
+      Scanner scan = new Scanner(System.in);
+      String scierzka = scan.next();
+
+
+      int count =0;
+      File file = new File(scierzka);
+      FileInputStream fis = new FileInputStream(file);
+      byte[] bytesArray = new byte[(int)file.length()];
+      fis.read(bytesArray);
+      String s = new String(bytesArray);
+      String [] data = s.split(" ");
+      for (int i=0; i<data.length; i++) {
+         count++;
+      }
+      System.out.println("Ilosc slow w pliku " +count);
+   }
+}
+ 
